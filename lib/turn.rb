eabf46f2e91223949b304d407ffdb1e2a14b4422
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-06-15 01:45:11 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,7 +36,7 @@ def turn(board)
   input = gets.strip
   input_to_index(input)
   if valid_move?(board, index) == true
-    move(board, index, token = "X")
+    return move(board, index, token = "X")
   
 
 end
